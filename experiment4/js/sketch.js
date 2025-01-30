1eefdcd6cfcbd359fb790da3a65d60c8cd26a4ad
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-01-30 14:57:41 -0800

    Piano implemented!

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -2,7 +2,39 @@
 // Author: Jack Sims
 // Date: 
 
+let pianoRef = {
+  Z:'A3',
+  X:'B3',
+  C:'C3',
+  V:'D3',
+  B:'E3',
+  N:'F3',
+  M:'G3',
+  A:'A4',
+  S:'B4',
+  D:'C4',
+  F:'D4',
+  G:'E4',
+  H:'F4',
+  J:'G4',
+  K:'A5',
+  L:'B5',
+  Q:'C5',
+  W:'D5',
+  E:'E5',
+  R:'F5',
+  T:'G5',
+  Y:'A6',
+  U:'B6',
+  I:'C6',
+  O:'D6',
+  P:'E6',
+}
+// TODO: Noise on SpaceBar
+// TODO: Let user adjust ADSR
+// TODO: Visual Component
 
+let polySynth;
 let soundProject = (p) => {
   p.setup = () => {
     // place our canvas, making it fit our container
@@ -12,10 +44,14 @@ let soundProject = (p) => {
       p.canvasContainer.height()
     );
     canvas.parent("canvas-container");
-    console.log("Works")
+    polySynth = new p5.PolySynth();
   };
 
   p.draw = () => {};
- 
+  p.keyTyped = () => {
+    p.userStartAudio();
+    polySynth.setADSR(0.01,0.1,1,0)
+    polySynth.play(pianoRef[p.key.toUpperCase()], 1,0,0.2);
+  }
 };
 new p5(soundProject);
