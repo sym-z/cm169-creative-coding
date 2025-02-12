f1a9a84df705582272d454182affd58bbc736931
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-02-12 10:50:23 -0800

    Input setup
    Credit for starting point: https://p5js.org/reference/p5/input/

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,19 +1,38 @@
-// sketch.js - 
+// sketch.js -
 // Author: Jack Sims
-// Date: 
+// Date:
+
+// Text input from the user.
+let input;
+
+// Center of the Canvas
+let centerW;
+let centerH;
 
 let textProject = (p) => {
   p.setup = () => {
-    // place our canvas, making it fit our container
     p.canvasContainer = $("#canvas-container");
     let canvas = p.createCanvas(
       p.canvasContainer.width(),
       p.canvasContainer.height()
     );
     canvas.parent("canvas-container");
+    p.background(200);
+
+    input = p.createInput("");
+    input.position(centerW, centerH + 100);
+
+    input.input(repaint);
+
+    centerW = p.canvasContainer.width()/2;
+    centerH = p.canvasContainer.height()/2;
   };
 
-  p.draw = () => {
-  };
+  function repaint() {
+    p.background(200);
+    let msg = input.value();
+    p.text(msg, centerW, centerH);
+  }
+  p.draw = () => {};
 };
 new p5(textProject);
