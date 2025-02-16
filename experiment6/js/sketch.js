9ba7525f29797c7a561b50bdc84d50912d5c0b13
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-02-16 12:22:02 -0800

    Basic input handling with enter key

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -9,6 +9,10 @@ let input;
 let centerW;
 let centerH;
 
+// For text formatting
+let wrapLength = 300;
+let fontSize = 32;
+
 let textProject = (p) => {
   p.setup = () => {
     p.canvasContainer = $("#canvas-container");
@@ -18,21 +22,37 @@ let textProject = (p) => {
     );
     canvas.parent("canvas-container");
     p.background(200);
-
+    makeInputBox();
+    formatSetup();
+    centerW = p.canvasContainer.width() / 2;
+    centerH = p.canvasContainer.height() / 2;
+  };
+  function makeInputBox() {
     input = p.createInput("");
     input.position(centerW, centerH + 100);
-
     input.input(repaint);
-
-    centerW = p.canvasContainer.width()/2;
-    centerH = p.canvasContainer.height()/2;
-  };
-
+  }
   function repaint() {
     p.background(200);
     let msg = input.value();
-    p.text(msg, centerW, centerH);
+    p.text(msg, centerW-wrapLength/2, centerH, wrapLength);
+  }
+  function formatSetup() {
+    p.textSize(fontSize);
+    p.textFont("consolas");
+    p.textAlign(p.CENTER, p.CENTER);
+    p.textWrap(p.WORD);
   }
   p.draw = () => {};
+  p.keyPressed = () => {
+    if (p.keyCode === p.ENTER) {
+      inputHandle(input.value());
+    }
+  };
+  function inputHandle(inText) {
+    console.log(inText);
+    input.value("");
+    repaint();
+  }
 };
 new p5(textProject);
